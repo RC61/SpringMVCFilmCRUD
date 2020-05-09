c7ad2ec251aff3514d9a65983f942c0a2e256387
Yettsy Knapp <yettsyknapp@SDMacBookPro3-2.local>
2020-05-09 15:08:01 -0600

    filmDAO

--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -7,13 +7,12 @@ import com.skilldistillery.film.entities.Film;
 
 public interface FilmDAO {
 	public Film findFilmbyID(int id) throws SQLException;
-<<<<<<< HEAD
+
 //	public List<Film> getFilmsBasedOnTitleOrDescription(String desc);
-	public Film addFilm(Film film);
-=======
+	
 	public List<Film> getFilmsBasedOnTitleOrDescription(String desc) throws SQLException;
 	public void addFilm(Film film) throws SQLException;
->>>>>>> d9326c1afabd2ae819eecd8113d7cf3b01f94173
+
 //	public void deleteFilm();
 //	public void editFilm();
 }
